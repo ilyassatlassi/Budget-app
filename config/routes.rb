6a7b3901f8a4c6f5eb4563b3cc062c8780b5d789
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-11 21:45:45 +0100

    Add category page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'home#index'
+
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+
+  resources :categories do
+    resources :transactions, only: %i[new create index destroy], on: :member
+  end
 end
