81f1a9c35e17e2fb43507846637a01372fd2f3e6
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-11 20:11:47 +0100

    Add function for categor controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,35 @@
 class CategoriesController < ApplicationController
-end
+    before_action :authenticate_user!
+  
+    def index
+      @categories = current_user.categories.includes(:expenses)
+    end
+  
+    def new
+      @category = Category.new
+    end
+  
+    def create
+      @category = current_user.categories.build(category_params)
+  
+      if @category.save
+        redirect_to categories_path, notice: 'Category was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def expenses
+      @category = current_user.categories.find(params[:category_id])
+      @expenses = @category.expenses.order(date: :desc)
+      @total_amount = @expenses.sum(:amount)
+  
+      render :index, locals: { expenses: @expenses, total_amount: @total_amount }
+    end
+  
+    private
+  
+    def category_params
+      params.require(:category).permit(:name, :icon)
+    end
+  end
