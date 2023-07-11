4ef7d74508ec379e8f608174a4da07f5f976d014
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-11 19:58:51 +0100

    create category controller znd views

--- app/models/category_transaction.rb
+++ app/models/category_transaction.rb
@@ -1,4 +1,4 @@
-class CategoryTransaction < ApplicationRecord
-    belongs_to :transaction, foreign_key: 'its_transaction_id'
-    belongs_to :category, foreign_key: 'category_id'
-  end
+# class CategoryTransaction < ApplicationRecord
+#     belongs_to :transaction, foreign_key: 'transaction_id'
+#     belongs_to :category, foreign_key: 'category_id'
+#   end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,16 @@
 class Transaction < ApplicationRecord
     belongs_to :author, class_name: 'User'
   
-    has_many :categories_transactions
-    has_many :categories, through: :transaction_categories
-  
+    belongs_to :category  
     validates :name, presence: true
     validates :amount, presence: true
+    validates :category, presence: true
+    validate :belongs_to_a_category
+
+    def belongs_to_a_category
+      errors.add(:category_id, 'must belong to a category') unless category_id && category
+    end
+
   end
 # class transactions < ApplicationRecord
 #     # belongs_to :category
