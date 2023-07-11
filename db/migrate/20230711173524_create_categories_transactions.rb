7eeebe60ad5e1d4861a2ea0223395f3452087727
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-11 18:44:54 +0100

    Add category_transactions table

--- db/migrate/20230711173524_create_categories_transactions.rb
+++
@@ -1,8 +0,0 @@
-class CreateCategoriesTransactions < ActiveRecord::Migration[7.0]
-  def change
-    create_table :categories_transactions do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_171742) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_174037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_171742) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_transactions", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "transaction_id", null: false
+    t.index ["category_id"], name: "index_categories_transactions_on_category_id"
+    t.index ["transaction_id"], name: "index_categories_transactions_on_transaction_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -48,6 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_171742) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "categories_transactions", "categories"
+  add_foreign_key "categories_transactions", "transactions"
   add_foreign_key "transactions", "categories"
   add_foreign_key "transactions", "users", column: "author_id"
 end
