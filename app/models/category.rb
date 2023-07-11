64fe2664779c70d250f1df60df2b8a0e20176519
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-11 19:23:42 +0100

    create medel for user

---
+++ app/models/category.rb
@@ -0,0 +1,11 @@
+class Category < ApplicationRecord
+    has_many :transactions
+    belongs_to :user
+  
+    validates :name, presence: true, length: { maximum: 30 }
+    validates :icon, presence: true
+  
+    def total_expense
+      transactions.sum(:amount)
+    end
+  end
---
+++ app/models/transaction.rb
@@ -0,0 +1,13 @@
+class transactions < ApplicationRecord
+    belongs_to :category
+    belongs_to :author, class_name: 'User'
+  
+    validates :name, presence: true
+    validates :category, presence: true
+    validates :amount, presence: true, numericality: { greater_than: 0 }
+    validate :belongs_to_a_category
+  
+    def belongs_to_a_category
+      errors.add(:category_id, 'must belong to a category') unless category_id && category
+    end
+  end
