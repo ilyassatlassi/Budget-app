6a134308a016c411a411963f26b04c6242304c92
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-11 19:34:13 +0100

    create categorytransacton  model

--- app/models/category_transaction.rb
+++ app/models/category_transaction.rb
@@ -1,4 +1,4 @@
-class categories_transactions < ApplicationRecord
+class CategoryTransaction < ApplicationRecord
     belongs_to :transaction, foreign_key: 'its_transaction_id'
     belongs_to :category, foreign_key: 'category_id'
   end
