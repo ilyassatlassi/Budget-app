83116cbaeb69682fd1cc290afc03235065f3666b
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-15 03:21:02 +0100

    fix an error

--- app/models/category_transaction.rb
+++ app/models/category_transaction.rb
@@ -1,4 +1,4 @@
-# class CategoryTransaction < ApplicationRecord
-#     belongs_to :transaction, foreign_key: 'transaction_id', dependent: :destroy
-#     belongs_to :category, foreign_key: 'category_id', dependent: :destroy
-#   end
+class CategoryTransaction < ApplicationRecord
+    # belongs_to :transaction, foreign_key: 'transaction_id', dependent: :destroy
+    # belongs_to :category, foreign_key: 'category_id', dependent: :destroy
+  end
